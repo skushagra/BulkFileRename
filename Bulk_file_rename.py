4ea2dcd0c626b0d76000412cdd0ce70802120a16
Kushagra S <kushagra.rigel@gmail.com>
2020-12-16 22:13:51 +0530

    Update Bulk_file_rename.py

--- Bulk_file_rename.py
+++ Bulk_file_rename.py
@@ -1,8 +1,8 @@
 import os
 ph = input("Enter Path :- ")
 files  = os.listdir(ph)
-for i in range(len(files)):
+for i in range(0,len(files)):
 	source = ph + files[i]
-	desti = ph + str(i+1)
+	desti = ph + str(i)
 	os.rename(source,desti)
 
